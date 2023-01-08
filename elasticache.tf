a433621eebc95ab474a0f264b5ada4908333b97d
eswar <eswar.awsdevops@gmail.com>
2023-01-08 12:31:25 +0530

    creating rds

--- elasticache.tf
+++ elasticache.tf
@@ -7,10 +7,11 @@ resource "aws_elasticache_subnet_group" "elasticache" {
   }
 }
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "${var.env}-elasticache"
-  engine               = var.engine
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  engine_version       = var.engine_version
-  port                 = 6379
-  az_mode              = var.az_mode
+  cluster_id      = "${var.env}-elasticache"
+  engine          = var.engine
+  node_type       = var.node_type
+  num_cache_nodes = var.num_cache_nodes
+  engine_version  = var.engine_version
+  port            = 6379
+  az_mode         = var.az_mode
+}
